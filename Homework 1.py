64375e97855388ed3c25d121e9b71bb9809dd0ab
Anna <211205annagmail.com@mail.ru>
2024-09-30 17:16:16 +0300

    inital

--- Homework 1.py
+++ Homework 1.py
@@ -1,7 +1,7 @@
 import threading
 import tarfile
 import yaml
-from console import Console  # Импортируем класс Console из другого файла
+from console import Console
 
 class Main:
     all_f = []
@@ -23,14 +23,15 @@ class Main:
         self.path = self.konf["path_vm"].replace(".tar", "") + "/"
         self.console.user = self.konf["user"]
 
-
-        # Для хранения прав доступа к файлам
+        # хранение прав доступа к файлам
         self.file_permissions = {}
 
-    # Методы обработки команд
+    # методы обработки команд
 
     def ls(self, append_path=""):
-        path = self.get_path(append_path)
+        path = self.get_append_path(append_path)
+        print(path)
+
         elems = []
 
         with tarfile.open(self.konf["path_vm"], "r") as tar:
@@ -65,80 +66,70 @@ class Main:
         self.console.path = self.path.replace(self.konf["path_vm"].replace(".tar", ""), "")
 
     def chmod(self, com, file):
-        file_main = self.find_file(file)
-
-        if file_main is None:
-            return f"File '{file}' not found."
-
         if com[0] in ['a', 'q', 'b'] and com[2] in ['r', 'w', 'x'] and com[1] in ['+', '-']:
+            # получаем путь файла
+            file_main = self.find_file(file)
+            if file_main is None:
+                return f"File {file} not found."
+
             who, oper, what = com[0], com[1], com[2]
             num_who = ['a', 'q', 'b'].index(who)
             num_what = ['r', 'w', 'x'].index(what)
 
             flag = False
-            # Проверяем права доступа к файлу
             if file not in self.file_permissions:
                 flag = True
-                self.file_permissions[file] = [1, 1, 1, 1, 1, 0, 0, 0, 0]  # по умолчанию все права разрешены
+                self.file_permissions[file] = [1, 1, 1, 1, 1, 0, 0, 0, 0]  # по умолчанию прописываем все права
 
             before = self.file_permissions[file][num_what]
-            print(self.file_permissions[file])
+
             if oper == '+':
                 self.file_permissions[file][num_who * 3 + num_what] = 1
             else:
                 self.file_permissions[file][num_who * 3 + num_what] = 0
 
-            # Проверяем, изменились ли права
-            print(self.file_permissions[file])
+            # проверяем, изменились ли права
             if before != self.file_permissions[file][num_what] or flag:
                 return "The rights have been successfully changed"
+            elif before == self.file_permissions[file][num_what] and not flag:
+                return "The file still has the same rights"
             return "An error occurred when changing permissions"
-
         return "The arguments were set incorrectly"
 
-    def tail(self, path):
-        path = self.get_path(path)[:-1]
+    def tail(self, file):
         with tarfile.open(self.konf["path_vm"], "r") as tar:
             for member in tar.getmembers():
-                if member.name == path and member.isfile():
-                    break
-            else:
-                return "No such file"
+                # аходим нужный нам файл в архиве
+                print(member.name)
+                print("path  ", self.path, "   file   ", file)
+                if member.name.endswith(file) and member.name[:len(member.name) - len(file)] == self.path and member.isfile():
+                    with tar.extractfile(member) as f: # октрываем для чтения
+                        text = (line.decode("utf-8") for line in f) # одновременное чтение и декодирование
+                        last_lines = list(text)[-10:]
+                        return "".join(last_lines)
 
-            with tar.extractfile(member) as f:
-                lines = f.readlines()
-                lines = [line.decode("utf-8") for line in lines]
-                return "".join(lines[-min(len(lines), 10):])
+        return "No such file"
 
     def clear(self):
         self.console.print("\n" * 20)
 
+    # вспомогательные методы
+    # возвращаем путь текущего файла в текстовом виде
     def find_file(self, file):
         with tarfile.open(self.konf["path_vm"], "r") as tar:
             for member in tar.getmembers():
-                print(f"Checking member: {member.name}")  # Отладочный вывод
-                if member.name.endswith(file):
+                if member.name.endswith(file) and member.name[:len(member.name) - len(file)] == self.path:
                     return member.name
-        print(f"File '{file}' not found.")  # Отладочный вывод
         return None
 
-    # --- Вспомогательные методы ---
-
-    def get_path(self, path):
-        path = path.split("/")
-        result_path = self.path
-        if path[0] == "" and len(path) > 1:
-            result_path = "./file_system/"
-            path = path[1:]
-        for elem in path:
-            if elem == "..":
-                result_path = "/".join(result_path.split("/")[:-2]) + "/"
-            elif elem == ".":
-                continue
-            else:
-                result_path += elem + "/"
-            result_path = result_path.replace("//", "/")
-        return result_path
+    # получение пути для команды ls
+    def get_append_path(self, append_file):
+        if append_file != "":
+            with tarfile.open(self.konf["path_vm"], "r") as tar:
+                for member in tar.getmembers():
+                    if member.name.endswith(append_file) and member.name.startswith(self.path):
+                        return member.name + "/"
+        return self.path
 
     def perform_command(self, command):
         command = command.split(" ")
@@ -171,26 +162,19 @@ class Main:
 
         self.console.insert_prompt()
 
-    def run_start_script(self):
-        if self.konf["start_script"]:
+    def work_with_script(self):
+        if self.konf["start_script"]: # проверка наличия скрипта
             with open(self.konf["start_script"], "r") as script:
-                self.console.insert_prompt()
+                self.console.insert_prompt() # вставка приглашения пользователя для ввода
                 for line in script:
                     line = line.strip()
                     self.console.print(line)
                     self.perform_command(line)
 
     def run(self):
-        start_cmds = threading.Thread(target=self.run_start_script)
-        start_cmds.start()
-
-        self.console.run()
-
-    def check(elem1, elem2):
-        for j in range(min(len(elem1), len(elem2))):
-            if elem1[j] != elem2[j]:
-                return False
-        return True
+        work_of_script = threading.Thread(target=self.work_with_script)
+        work_of_script.start()
+        self.console.run() # запускаем цикл интерфейса
 
 if __name__ == "__main__":
     task = Main()
